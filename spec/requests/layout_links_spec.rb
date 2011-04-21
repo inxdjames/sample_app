10316b7a3ff13bb84b93f28adbb78f67db9be10f
Doug James <djames@intranexus.com>
2011-04-21 12:01:50 -0400

    finished layout and routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  it "has a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "has a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "has an About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "has a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
+
+  it "has a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+end
+

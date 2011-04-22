b4a845d7f5e7f584992b3ffa1d6e0a9b5017a972
Doug James <djames@intranexus.com>
2011-04-22 13:57:54 -0400

    user model with password and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,6 +13,37 @@ describe UsersController do
       get "new"
       response.should have_selector("title", :content => "Sign up")
     end
+  end
+
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "succeeds" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "finds the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+
+    it "has the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "includes the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "has a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
 
   end
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+Factory.define :user do |user|
+  user.name                  "Doug James"
+  user.email                 "djames@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
+
+
